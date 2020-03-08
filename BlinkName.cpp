4147957d21b8f817a05b3db5c018e003bcf4e194
Nick Yazdani <nnyazdani92@gmail.com>
2020-03-09 10:07:29 +1100

    first commit

---
+++ BlinkName.cpp
@@ -0,0 +1,42 @@
+int led = D7;
+char myMorse[] = {'-', '.', '-', '-', '.', '-', '-', '-','.','.','.','-','-','.','.','.'};
+
+void longSequence()
+{
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(1000);
+}
+
+void shortSequence()
+{
+  digitalWrite(led, HIGH);
+  delay(300);
+  digitalWrite(led, LOW);
+  delay(300);
+}
+
+
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+ 
+  //so for the name Yazdani we need:
+  //-.-- .- --.. -.. .- -. ..
+  for(int i = 0; i < sizeof(myMorse)/sizeof(myMorse[0]); i++)
+  {
+      if(myMorse[i] == '-')
+      {
+          longSequence();
+      }
+      else
+      {
+          shortSequence();
+      }
+  }
+
+}
+
